86003fab467ca44eba24e356b83079515fc7683f
PiotrMc <77527394+PiotrMc@users.noreply.github.com>
2021-02-02 17:49:47 +0100

    1

---
+++ 1.Początki/script.js
@@ -0,0 +1,14 @@
+
+console.log("Piotr");
+
+
+
+console.log("12 + 32 = "+ (12+ 32));
+console.log("52 / 16 = "+ (52 / 16));
+console.log("5 * 2 = "+ (5 * 2));
+
+
+console.log("Hello World");
+//console.log(Hello World); //SyntaxError: missing ) after argument list
+//console.log("Hello World); //SyntaxError: Invalid or unexpected token
+
